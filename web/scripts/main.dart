2c9881863325233284c857cd56cf3472963f1a60
Jenny <gtg287y@gmail.com>
2018-02-07 00:43:06 -0500

    yes

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -1,13 +1,16 @@
 import 'dart:html';
 import "TranscriptParser.dart";
+import 'random.dart';
 Element output;
 void main() {
   //querySelector('#output').text = 'Your Dart app is running.';
   output = querySelector('#output');
   //TODO have it read a random transcript
-  int maxNumber = 1;
-  int number = 2;
+  int maxNumber = 4;
+  int number = new Random().nextInt(maxNumber)+1;
+  number = 4;
   //http://readmspa.org/transcripts/
   TranscriptParser.init(); //static vars don't exist till they do
+  output.text = ("Script: $number");
   TranscriptParser.readTranscript("transcripts/$number.txt",output);
 }
