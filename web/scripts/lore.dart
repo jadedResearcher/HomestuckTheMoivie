dd1836a114caee052fb089fe0e6e674944d0642e
Jenny <gtg287y@gmail.com>
2018-03-29 14:14:07 -0400

    small changes

--- web/scripts/lore.dart
+++ web/scripts/lore.dart
@@ -14,4 +14,16 @@ void main() {
   TranscriptParser.init(); //static vars don't exist till they do
   output.text = ("Script: $number");
   TranscriptParser.readTranscript("transcripts/$number.txt",output);
+  storeCard("N4Igzg9grgTgxgUxALhAJQQQwCYAIASEAtgmAC5RwDWuZAFgrgLIQCWAbkgDQgB2mJFCCaYqjTLgDKdVmTIBPZm04geZBAA8yQ3BADuvBDAT8SMAORhcrGBF6s4uPQgBGcYg9wuE7klb2sADbYgYoADhBhUIGYMLiYvHgSLOysCAB0memacFBgjCRczADyAJIAagCiuADmCGRWRDiMEABm1mTpuAAqDIqxjLz6uGilAML4uACCAHIAIrgAYlNMxQCqkumqIC6Y1DW2UIkzAkioAOIQENgivGCsdulhvDXbZDCsNXUwY3QJiEIAAzpACs2zAiEMYG6EDWvECEGoQgA2gBdbbGMDRBqSMiYBoo4AAHT4pxJyBJyyYlRJXBJ7EwgSgCHJJIAjICSQBfIrE0kkVkgABMIIAbLgNLgRaLafTGczBdLubySaYWSgSWNikxqWgxjTVHKmeqKSAOdz0WoPl8jLj8WAADKkfIwFGWkDvT7fO0Nc7GfFGQmqska4ViiVSsWykAM42CtkWt7W714hqVACOUEZbpAXKAA");
+}
+
+void storeCard(String card) {
+  String key = "LIFESIMFOUNDCARDS";
+  if(window.localStorage.containsKey(key)) {
+    String existing = window.localStorage[key];
+    List<String> parts = existing.split(",");
+    if(!parts.contains(card)) window.localStorage[key] = "$existing,$card";
+  }else {
+    window.localStorage[key] = card;
+  }
 }
